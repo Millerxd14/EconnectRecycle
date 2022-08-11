c2c7e5100735b6583b637e837c3beef021e5186a
Millerxd14 <yozombie34@gmail.com>
2022-08-10 22:16:04 -0500

    pycache

--- users/views.py
+++ users/views.py
@@ -63,6 +63,7 @@ def recuperar_contrasena(request):
     pass
 
 def update_profile(request):
+    #import pdb;pdb.set_trace()
     profile = request.user.profile
     if request.method == 'POST':
         form = ProfileForm(request.POST, request.FILES)
